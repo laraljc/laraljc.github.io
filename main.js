5fbc0fb2be1cca39488a9c047019c3aeeac756fa
Lara Chu <lara.chu@berkeley.edu>
2020-07-15 22:45:36 -0700

    colored rearranged

--- main.js
+++ main.js
@@ -1,27 +1,86 @@
-var modal = document.getElementById('simplemodal');
+// Modals for Experiences: exp1, exp2, exp3
+var exp1 = document.getElementById('simplemodal');
+var exp2 = document.getElementById('exp2');
+var exp3 = document.getElementById('exp3');
+
+var proj1 = document.getElementById('projmod1');
+var proj2 = document.getElementById('projmod2');
+var proj3 = document.getElementById('projmod3');
+
+// Cards
 var projectCard1 = document.getElementsByClassName('project-card')[0];
 var projectCard2 = document.getElementsByClassName('project-card')[1];
 var projectCard3 = document.getElementsByClassName('project-card')[2];
 
-var closebutton = document.getElementsByClassName('closebutton')[0];
+var projectCard4 = document.getElementsByClassName('project-card')[3];
+var projectCard5 = document.getElementsByClassName('project-card')[4];
+var projectCard6 = document.getElementsByClassName('project-card')[5];
 
-projectCard1.addEventListener('click', openModal);
-projectCard2.addEventListener('click', openModal);
-projectCard3.addEventListener('click', openModal);
+// Close Buttons
+var closebutton1 = document.getElementsByClassName('closebutton')[0];
+var closebutton2 = document.getElementsByClassName('closebutton')[1];
+var closebutton3 = document.getElementsByClassName('closebutton')[2];
+var closebutton4 = document.getElementsByClassName('closebutton')[3];
+var closebutton5 = document.getElementsByClassName('closebutton')[4];
+var closebutton6 = document.getElementsByClassName('closebutton')[5];
 
-closebutton.addEventListener('click', closeModal);
+
+// Make Cards Clickable
+projectCard1.addEventListener('click', openModal1);
+projectCard2.addEventListener('click', openModal2);
+projectCard3.addEventListener('click', openModal3);
+
+projectCard4.addEventListener('click', openModal4);
+projectCard5.addEventListener('click', openModal5);
+projectCard6.addEventListener('click', openModal6);
+
+// Make Close Buttons Clickable
+closebutton1.addEventListener('click', closeModal);
+closebutton2.addEventListener('click', closeModal);
+closebutton3.addEventListener('click', closeModal);
+closebutton4.addEventListener('click', closeModal);
+closebutton5.addEventListener('click', closeModal);
+closebutton6.addEventListener('click', closeModal);
+
+
+// To Exit Modal Click Window
 window.addEventListener('click', clickOutside);
 
-function openModal() {
-  modal.style.display = 'block';
+function openModal1() {
+  exp1.style.display = 'block';
+}
+
+function openModal2() {
+  exp2.style.display = 'block';
+}
+
+function openModal3() {
+  exp3.style.display = 'block';
+}
+
+function openModal4() {
+  proj1.style.display = 'block';
+}
+
+function openModal5() {
+  proj2.style.display = 'block';
+}
+
+function openModal6() {
+  proj3.style.display = 'block';
 }
 
 function closeModal() {
-  modal.style.display = 'none';
+  exp1.style.display = 'none';
+  exp2.style.display = 'none';
+  exp3.style.display = 'none';
+  proj1.style.display = 'none';
+  proj2.style.display = 'none';
+  proj3.style.display = 'none';
 }
 
 function clickOutside(e) {
   if(e.target == modal){
-    modal.style.display = 'none';
+    exp1.style.display = 'none';
   }
 }
